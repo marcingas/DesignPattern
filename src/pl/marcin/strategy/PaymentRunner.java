d025767245a9e656b6657ca2b07f42bab369612e
Marcin <gasiorek.mail@gmail.com>
2023-04-04 18:36:31 +0200

    change names of variables

--- src/pl/marcin/strategy/PaymentRunner.java
+++ src/pl/marcin/strategy/PaymentRunner.java
@@ -1,23 +1,20 @@
 package pl.marcin.strategy;
 
-import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
-
 public class PaymentRunner {
     public static void main(String[] args) {
         PaymentForm blik = new Blik();
         PaymentForm creditCard = new CreditCardPayment();
         PaymentForm internetBankTransfer = new InternetBankTransfer();
 
-        paymentPrinter(blik,300);
-        paymentPrinter(creditCard,300);
-        paymentPrinter(internetBankTransfer,300);
+        processPaymentPrinter(blik,300);
+        processPaymentPrinter(creditCard,300);
+        processPaymentPrinter(internetBankTransfer,300);
 
 
 
     }
-    public static void paymentPrinter(PaymentForm form, int amount){
-        System.out.println(form.processPayment(amount));
+    public static void processPaymentPrinter(PaymentForm paymentForm, int amount){
+        System.out.println(paymentForm.processPayment(amount));
     }
 
 
