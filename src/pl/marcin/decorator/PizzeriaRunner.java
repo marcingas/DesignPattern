c67848df3db2321576b9db783b593f0129da647d
Marcin <gasiorek.mail@gmail.com>
2023-04-05 13:06:04 +0200

    diffrent ways of running code

--- src/pl/marcin/decorator/PizzeriaRunner.java
+++ src/pl/marcin/decorator/PizzeriaRunner.java
@@ -14,13 +14,16 @@ public class PizzeriaRunner {
 //        System.out.println("Third addition: Bacon");
 //        margerita= new DecoratorBacon(margerita);
 //        methodRunner(margerita);
-//        Other way of instantiate it :
+//        Other ways of instantiate it :
         Pizza margerita = new BasePizza("Margerita",20);
         margerita = new DecoratorBacon(new DecoratorCheese(new DecoratorHam(margerita)));
         methodRunner(margerita);
         Pizza quattro = new BasePizza("Quattro",15);
         quattro = new DecoratorCheese(new DecoratorHam(new DecoratorBacon(quattro)));
         methodRunner(quattro);
+        Pizza italiana = new BasePizza("Italiana",60);
+        methodRunner(new DecoratorBacon(new DecoratorCheese(new DecoratorBacon(italiana))));
+        methodRunner(new DecoratorBacon(new DecoratorCheese(new BasePizza("French",45))));
     }
 
     public static void methodRunner(Pizza pizza){
